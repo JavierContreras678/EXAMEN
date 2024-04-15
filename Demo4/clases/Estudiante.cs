08622f1d1151dd4ae6f1d99fc94e8e6be7a13923
JavierContreras678 <javierajalex2@gmail.com>
2024-04-15 11:27:52 -0600

    Agregar archivos de proyecto.

---
+++ Demo4/clases/Estudiante.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Demo4.clases
+{
+    public class Estudiante
+    {
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [MaxLength(100, ErrorMessage = "El máximo de caracteres es 100")]
+        public string? Nombre { get; set; }
+
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        [EmailAddress(ErrorMessage = "Correo inválido")]
+        [MaxLength(100, ErrorMessage = "El máximo de caracteres es 100")]
+        public string? Correo { get; set; }
+
+        [Required(ErrorMessage = "La calificación es obligatoria")]
+        public int? Calificacion { get; set; }
+    }
+}
