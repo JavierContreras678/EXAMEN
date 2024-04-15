88b650e5b5d7994f642c5767f78953e887e1c0cf
Javier <117784518+JavierContreras678@users.noreply.github.com>
2024-04-15 11:30:07 -0600

    Add files via upload

---
+++ Producto.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.ComponentModel.DataAnnotations;
+namespace Demo4.Productos
+{
+    public class Producto
+    {
+        [Required(ErrorMessage = "El número de inventario es obligatorio")]
+        [Range(1, int.MaxValue, ErrorMessage = "El número de inventario debe ser un número positivo mayor a 0")]
+        public string? Inventario { get; set; }
+
+
+        [Required(ErrorMessage = "El nombre del producto es obligatorio")]
+        [MaxLength(200, ErrorMessage = "El máximo de caracteres es 200")]
+        public string? Nombre { get; set; }
+
+        [Required(ErrorMessage = "El Departamento es obligatorio")]
+        public string? Departamento { get; set; }
+
+
+
+    }
+}
